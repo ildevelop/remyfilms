97f9b7133b922900bb103379a6fce02e1a744280
ildevelop <raduilya@gmail.com>
2019-02-04 05:34:37 +0200

    [build]

---
+++ build/precache-manifest.abd6d64950e37e27db16f89395da8b83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "84c1218755cac17e88c6",
+    "url": "/static/js/main.84c12187.chunk.js"
+  },
+  {
+    "revision": "80b05586f955294ed10b",
+    "url": "/static/js/1.80b05586.chunk.js"
+  },
+  {
+    "revision": "84c1218755cac17e88c6",
+    "url": "/static/css/main.a67432c1.chunk.css"
+  },
+  {
+    "revision": "4776edc1f8057bbdd677c7c000bc6e8f",
+    "url": "/index.html"
+  }
+];
